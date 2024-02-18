631839e2a41a092356cc711d15603d76e9266d90
Jayaku Briliantio <jayakubriliantio@gmail.com>
2024-02-19 03:14:29 +0700

    feat: new sidebar homework

---
+++ class/sidebar/script.js
@@ -0,0 +1,7 @@
+function openSidebar() {
+  document.getElementById("sidebar").style.display = "flex";
+}
+
+function closeSidebar() {
+  document.getElementById("sidebar").style.display = "none";
+}
